0f10a6a08deadf94a31d49df4193ed09beb22948
Isa.Iraz <isa.iraz74@gmail.com>
2022-01-27 10:46:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const taskManager = () => {
+    let seeTasks, addTasks, deleteTask, markTask, exit;
+    let enter;
+    
+
+    console.log("Welcome to your task manager, Press:  1. to see all your tasks  2. to add a task  3. to delete a task  4. to mark a task as done  5. to Exit the task manager");
+
+    enter = window.prompt('Enter a value for enter');
+
+    if (enter == 1) {
+        console.log(seeTasks);
+
+    } else if (enter == 2) {
+         addTasks = window.prompt('Enter a value for addTasks');
+
+        } else if (enter == 3) {
+    
+            }
+        }
+
+
+
+
+taskManager()
