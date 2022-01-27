f05448236e95761f101b9eb66eb0f8fa87651865
Isa.Iraz <isa.iraz74@gmail.com>
2022-01-27 14:50:44 +0100

    do the first and second of the work

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
+const { clear } = require('console')
+const readline = require('readline')
+const rl = readline.createInterface({input : process.stdin,
+                                    output : process.stdout})
+
+const menu = () => {
+    rl
+}
 const taskManager = () => {
-    let seeTasks, addTasks, deleteTask, markTask, exit;
-    let enter;
-    
-
-    console.log("Welcome to your task manager, Press:  1. to see all your tasks  2. to add a task  3. to delete a task  4. to mark a task as done  5. to Exit the task manager");
-
-    enter = window.prompt('Enter a value for enter');
-
-    if (enter == 1) {
-        console.log(seeTasks);
-
-    } else if (enter == 2) {
-         addTasks = window.prompt('Enter a value for addTasks');
-
-        } else if (enter == 3) {
-    
-            }
+    let allTasks =[]
+    rl.question("Welcome to your task manager Press:\n 1. to see all your tasks\n 2. to add a task\ 3. to delete a task\n 4. to mark a task as done\n 5. to Exit the task manager \n", (userInput) => {
+        if (userInput == "1") {
+            clear()
+            console.log( " here are all you're tasks: \n" + allTasks);
         }
+        else if (userInput == "2") {
+            clear()
+            rl.question("add you're task : ", (userInput) => {
+                allTasks = userInput
+                console.log("new task added: " + userInput)
+            })
+        
+        }
+       
 
-
+      });
+  
+}
 
 
 taskManager()
